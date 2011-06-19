eee04570ec3fced3c845a7bfddb2c9dc04f8f38b
Chris McGrath <mcgrath.chris@gmail.com>
2011-06-18 23:51:16 -0500

    + Add subscriber_edit

--- README.md
+++ README.md
@@ -49,6 +49,20 @@ session.subscriber_add(
 )
 ```
 
+### Edit
+```ruby
+session.subscriber_edit(
+	:subscriber_id => 12345, # must include if finding by subscriber_id
+	:list_id       => 12345, # must include if finding by list_id and email
+	:email         => 'test@test.com', # must include if finding by list_id and email
+	:account_id    => 1044867, # must include if using s4
+	:attributes    => {
+		'Email__Address' => 'test1@test.com',
+		'First__Name'    => 'Test'
+	}
+)
+```
+
 ### Delete
 ```ruby
 session.subscriber_delete(
@@ -80,3 +94,6 @@ Subscribers
 Code
 ----
 * Rework XML generation
+* Merge subscriber_delete and subscriber_delete_from_list
+* Add example with attributes for subscriber_add and subsciber_edit
+* Fix subscriber_add account_id
--- lib/etapi/calls/subscriber.rb
+++ lib/etapi/calls/subscriber.rb
@@ -40,7 +40,7 @@ module ETAPI
           xml.values do
             xml.Email__Address @email
             xml.status "active"
-            @attributes.each do|name, value|
+            @attributes.each do |name, value|
               eval("xml.#{name} '#{(value.is_a?(Array)) ? value.join(',') : value}'")
             end
           end
@@ -58,6 +58,49 @@ module ETAPI
       subscriber_msg    = response.xpath("//subscriber_info")
       
       ETAPI.log("    Subscriber ID:      #{subscriber_id.text.to_i}\n    Subscriber Message: #{subscriber_msg.text}")
+      
+    end
+    
+    def subscriber_delete(*args)
+      #a.subscriber_delete(:subscriber_id => 241224046)
+      
+      # options
+      options         = args.extract_options!
+      @subscriber_id  = options[:subscriber_id]
+      
+      # check for required options
+      raise ArgumentError, "* missing :subscriber_id *" if @subscriber_id.blank?
+      
+      # convert options
+      @subscriber_id = @subscriber_id.to_i
+      
+      # build xml data
+      data = ""
+      xml = Builder::XmlMarkup.new(:target => data, :indent => 2)
+      xml.instruct!
+      xml.exacttarget do
+        xml.authorization do
+          xml.username @username
+          xml.password @password
+        end
+        xml.system do
+          xml.system_name "subscriber"
+          xml.action "delete"
+          xml.search_type "subid"
+          xml.search_value @subscriber_id
+          xml.search_value2 nil
+        end
+      end
+      
+      data_encoded = "qf=xml&xml=" + url_encode(data)
+      
+      response = @api_url.post(@api_uri.path, data_encoded, @headers.merge('Content-length' => data_encoded.length.to_s))
+      check_response(response)
+      response = Nokogiri::XML::Document.parse(response.read_body)
+      
+      subscriber_msg    = response.xpath("//subscriber_info")
+      
+      ETAPI.log("    Subscriber Message: #{subscriber_msg.text}")
       
     end
     
@@ -106,18 +149,26 @@ module ETAPI
       
     end
     
-    def subscriber_delete(*args)
-      #a.subscriber_delete(:subscriber_id => 241224046)
+    def subscriber_edit(*args)
+      #a.subscriber_edit(:subscriber_id => 241224046, :account_id => 1044867, :attributes => {'Email__Address' => 'test1@test.com', 'First__Name' => 'Chris'})
+      #a.subscriber_edit(:list_id => 111247, :email => 'test@test.com')
       
       # options
       options         = args.extract_options!
       @subscriber_id  = options[:subscriber_id]
+      @list_id        = options[:list_id]
+      @email          = options[:email]
+      @attributes     = options[:attributes] ||= []
+      @account_id     = options[:account_id]
       
       # check for required options
-      raise ArgumentError, "* missing :subscriber_id *" if @subscriber_id.blank?
+      raise ArgumentError, "* missing :subscriber_id || (:list_id && :email) *" if (@subscriber_id.blank? && @list_id.blank? && @email.blank?) || (@subscriber_id.blank? && (@list_id.blank? || @email.blank?))
+      raise ArgumentError, "* missing :account_id | [using s4] *" if ETAPI.use_s4 && @account_id.blank?
       
       # convert options
-      @subscriber_id = @subscriber_id.to_i
+      @subscriber_id  = @subscriber_id.to_i unless @subscriber_id.blank?
+      @list_id        = @list_id.to_i unless @list_id.blank?
+      @account_id     = @account_id.to_i unless @account_id.blank?
       
       # build xml data
       data = ""
@@ -130,10 +181,16 @@ module ETAPI
         end
         xml.system do
           xml.system_name "subscriber"
-          xml.action "delete"
-          xml.search_type "subid"
-          xml.search_value @subscriber_id
-          xml.search_value2 nil
+          xml.action "edit"
+          xml.search_type @subscriber_id.blank? ? "listid" : "subid"
+          xml.search_value @subscriber_id.blank? ? @list_id : @subscriber_id
+          xml.search_value2 @email
+          xml.values do
+            @attributes.each do |name, value|
+              eval("xml.#{name} '#{(value.is_a?(Array)) ? value.join(',') : value}'")
+            end
+            xml.ChannelMemberID @account_id if ETAPI.use_s4
+          end
         end
       end
       
@@ -143,9 +200,10 @@ module ETAPI
       check_response(response)
       response = Nokogiri::XML::Document.parse(response.read_body)
       
+      subscriber_id     = response.xpath("//subscriber_description")
       subscriber_msg    = response.xpath("//subscriber_info")
       
-      ETAPI.log("    Subscriber Message: #{subscriber_msg.text}")
+      ETAPI.log("    Subscriber ID:      #{subscriber_id.text.to_i}\n    Subscriber Message: #{subscriber_msg.text}")
       
     end
     
