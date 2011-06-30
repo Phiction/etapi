b7c28bf6028ae22a20b844ef9cd759ddaff21083
Chris McGrath <mcgrath.chris@gmail.com>
2011-06-29 23:59:39 -0500

    + Config update

--- lib/etapi/configuration.rb
+++ lib/etapi/configuration.rb
@@ -23,39 +23,26 @@ module ETAPI
       :log
     )
     
-    # Creates ETAPI.log = {value}
     attr_writer(:log)
-    
-    # Returns whether to log. (true)
     def log?
      @log != false
     end
     
-    # Creates ETAPI.logger = {value}
-    attr_writer(:logger)
-    
-    # Returns the logger. (::Logger.new(STDOUT))
-    def logger
-      @logger ||= ::Logger.new(STDOUT)
-    end
-    
-    # Creates ETAPI.log_level = {value}
-    attr_writer(:log_level)
-    
-    # Returns the log level. (:debug)
-    def log_level
-      @log_level ||= :debug
-    end
-    
-    # Logs the passed message.
     def log(message)
      logger.send(log_level, "\n#{message}\n") if log?
     end
     
-    # Creates ETAPI.log = {value}
-    attr_writer(:raise_errors)
+    attr_writer(:logger)
+    def logger
+      @logger ||= ::Logger.new(STDOUT)
+    end
     
-    # Returns whether to raise errors. (true)
+    attr_writer(:log_level)
+    def log_level
+      @log_level ||= :debug
+    end
+    
+    attr_writer(:raise_errors)
     def raise_errors?
       @raise_errors != false
     end
